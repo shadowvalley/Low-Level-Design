4547a4b80e4d33988254cf09d9b5c9d81d176c0f
Satyam Singh <ssatsin2308@gmail.com>
2023-12-13 13:07:14 +0530

    Adding call for TicTacToe class from main

--- TicTacToe/src/Main.java
+++ TicTacToe/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Tic Tac Toe!");
+        TicTacToe game = new TicTacToe();
+        game.initialiseGame();
+        System.out.println("game winner is: " + game.startGame());
     }
 }
