c62f55ea705a4ae3b30aab3be42fdcaae608bbf5
Satyam Singh <ssatsin2308@gmail.com>
2023-12-13 10:42:27 +0530

    Removing lombok @AllArgsConstructor with manual constructor

--- Elevator/src/Request/Request.java
+++ Elevator/src/Request/Request.java
@@ -1,15 +1,19 @@
 package Request;
 
-import lombok.AllArgsConstructor;
-
 /**
  * Elevator Request Class
  * */
 
-@AllArgsConstructor
 public class Request {
     public int currentFloor;
     public int desiredFloor;
     public Location elevatorState;
     public Direction elevatorDirection;
+
+    public Request(int currentFloor, int desiredFloor, Location location, Direction direction) {
+        this.currentFloor = currentFloor;
+        this.desiredFloor = desiredFloor;
+        this.elevatorState = location;
+        this.elevatorDirection = direction;
+    }
 }
